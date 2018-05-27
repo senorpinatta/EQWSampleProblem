c7453786c318ed9266301479beaa0a88ad90edee
senorpinatta <12atrf@queensu.ca>
2018-05-27 13:35:51 -0400

    Final cleaning and formatting of code and comments

--- pysparkJob.py
+++ pysparkJob.py
@@ -8,16 +8,22 @@ import sys
 
 if __name__ == "__main__":
 
-    sc = SparkContext(appName="Hello Spark")
+    sc = SparkContext(appName="SparkProblem")
 
     sqlContext = SQLContext(sc)
 
+    #Load csv into dataframe
     df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('DataSample.csv')
 
+    #Remove suspicious requests (ie records with identical fields)
     df_no_dupes = df.dropDuplicates(['TimeSt','Latitude','Longitude'])
+    no_dupes_length = df_no_dupes.count()-1
 
+    #The points of interest (poi)
     poiList = [(53.546167000000004, -113.48573400000001), (45.521629, -73.566024), (45.22483, -63.232729000000006)]
 
+    #The following three functions return a single rdd containing the distance between each row in 
+    #DataSample.csv and one of the POI coordinates. distance1 for POI1, distance2 for POI2, distance3 for POI3.
     def distance1(row):
         return float(np.sqrt((row.Latitude-poiList[0][0])**2+(row.Longitude-poiList[0][1])**2))
 
@@ -27,45 +33,49 @@ if __name__ == "__main__":
     def distance3(row):
         return float(np.sqrt((row.Latitude-poiList[2][0])**2+(row.Longitude-poiList[2][1])**2))
 
-
+    #Retrieve the dataframes of distances for each POI.
     d1rdd = df_no_dupes.rdd.map(lambda x: distance1(x))
-    d1rdd.collect()
     d2rdd = df_no_dupes.rdd.map(lambda x: distance2(x))
-    d2rdd.collect()
     d3rdd = df_no_dupes.rdd.map(lambda x: distance3(x))
-    d3rdd.collect()
 
+    #Turn each rdd into a list.
     d1List = [i for i in d1rdd.collect()]
     d2List = [i for i in d2rdd.collect()]
     d3List = [i for i in d3rdd.collect()]
 
+    #List that holds the string "POI_1/2/3" acting as labels for each record, identifying the closest POI
     labelCol = []
-    distanceCol = []
-    radius1, radius2, radius3 = 0, 0, 0
-    d1Count, d2Count, d3Count = 0, 0, 0
+    
+    #List holding the distance value of each request record and its closest POI
+    distanceCol = [] 
+    
+    radius1, radius2, radius3 = 0, 0, 0 #Radius values for each POI
+    d1Count, d2Count, d3Count = 0, 0, 0 #Counters for each POI
+    
+    #Values that will eventually hold the average distance between POIs and records within their area.
     d1Avg, d2Avg, d3Avg = 0, 0, 0
 
-    def magic(labelCol, distanceCol):
+    def distanceStats(labelCol, distanceCol):
         global radius1, radius2, radius3, d1Count, d2Count, d3Count, d1Avg, d2Avg, d3Avg
-        for x in range(0,19998):
+        for x in range(0, no_dupes_length-1):
             d1 = d1List[x]
             d2 = d2List[x]
             d3 = d3List[x]
-            if d1 < d2 and d1 < d3:
+            if d1 < d2 and d1 < d3:     #If POI1 is the closest
                 if d1 > radius1:
                     radius1 = d1
                 labelCol.append("POI1")
                 distanceCol.append(d1)
                 d1Count += 1
                 d1Avg += d1
-            elif d2 < d1 and d2 < d3:
+            elif d2 < d1 and d2 < d3:   #If POI2 is the closest
                 if d2 > radius2:
                     radius2 = d2
                 labelCol.append("POI2")
                 distanceCol.append(d2)
                 d2Count += 1
                 d2Avg += d1
-            else:
+            else:                       #If POI3 is the closest
                 if d3 > radius3:
                     radius3 = d3
                 labelCol.append("POI3")
@@ -76,9 +86,12 @@ if __name__ == "__main__":
         d2Avg = d2Avg/d2Count
         d3Avg = d3Avg/d3Count
 
-    magic(labelCol, distanceCol)
+    distanceStats(labelCol, distanceCol)
 
+    #Standard deviation values for each POI
     poi1SD, poi2SD, poi3SD = 0, 0, 0
+    
+    #Standard deviation function
     def SDCalc():
         global poi1SD, poi2SD, poi3SD
         for x in range(0, 19998):
@@ -101,18 +114,14 @@ if __name__ == "__main__":
     density1 = d1Count/area1
     density2 = d2Count/area2
     density3 = d3Count/area3
-
-    area1 = np.pi * (radius1**2)
-    area2 = np.pi * (radius2**2)
-    area3 = np.pi * (radius3**2)
     
-    density1 = d1Count/area1
-    density2 = d1Count/area2
-    density3 = d1Count/area3
+    #Print all notable statistics and values to text file
+    print("POI_radii: ", radius1, radius2, radius3, "\nPOI_count: ", d1Count, d2Count, d3Count, "\nPOI_Avg: ", d1Avg, d2Avg, d3Avg, "\nPOI_STD: ", poi1SD, poi2SD, poi3SD, "\nPOI_Area: ", area1, area2, area3, "\nPOI_density: ", density1, density2, density3, file=open("output.txt", "w"))
 
-    circle1 = plt.Circle((poiList[0][0], poiList[0][1]), radius=radius1, color='r')
-    circle2 = plt.Circle((poiList[1][0], poiList[1][1]), radius=radius2, color='g')
-    circle3 = plt.Circle((poiList[2][0], poiList[2][1]), radius=radius3, color='b')
+    #Finally, create and plot representative POI areas
+    circle1 = plt.Circle((poiList[0][0], poiList[0][1]), radius=radius1, color='r') #POI1
+    circle2 = plt.Circle((poiList[1][0], poiList[1][1]), radius=radius2, color='g') #POI2
+    circle3 = plt.Circle((poiList[2][0], poiList[2][1]), radius=radius3, color='b') #POI3
     
     ax = plt.gca()
     ax.cla()
@@ -121,8 +130,6 @@ if __name__ == "__main__":
     ax.add_patch(circle1)
     ax.add_patch(circle2)
 
-    print("POI_radii: ", radius1, radius2, radius3, "\nPOI_count: ", d1Count, d2Count, d3Count, "\nPOI_Avg: ", d1Avg, d2Avg, d3Avg, "\nPOI_STD: ", poi1SD, poi2SD, poi3SD, "\nPOI_Area: ", area1, area2, area3, "\nPOI_density: ", density1, density2, density3, file=open("output.txt", "w"))
-    
     plt.axis('scaled')
     plt.show()
 
